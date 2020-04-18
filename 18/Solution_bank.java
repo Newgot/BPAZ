1eb58a198eee32044378d1e2377b3005d585879d
Vladistal Bakhov <kreazilist@gmail.com>
2020-04-19 00:06:04 +0700

    Лабораторная 3 (Исправленная)

--- 18/Solution_bank.java
+++ 18/Solution_bank.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class Solution {
 
@@ -29,15 +30,14 @@ class Solution {
      * {1 2 3 4}, m = 2.5, 2 * 2.5 > 4 0
      * Ответ: 0
      */
-
     // Пузырьковая сортировка
-    public static void sort(double[] arr){
-        for(int i = arr.length-1 ; i > 0 ; i--){
-            for(int j = 0 ; j < i ; j++){
-                if( arr[j] > arr[j+1] ){
+    public static void sort(double[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
                     double tmp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tmp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
 
                 }
             }
@@ -49,28 +49,28 @@ class Solution {
     // Complete the activityNotifications function below.
     static int activityNotifications(int[] expenditure, int d) {
         int n = expenditure.length;
-        if (n > d){
+        if (n > d) {
             double[] period = new double[d]; // Массив для подсчкета медианы
             int count = 0;
-            for (int i = 0;i + d < n;i++){
-                for (int j = 0; j  < d; j++){
+            for (int i = 0; i + d < n; i++) {
+                for (int j = 0; j < d; j++) {
                     period[j] = expenditure[i + j];
                 }
                 sort(period);
                 double median; // Медиана
-                if (d % 2 == 0){ // Медиана у массива с четным колличеством элементов
+                if (d % 2 == 0) { // Медиана у массива с четным колличеством элементов
                     median = period[d / 2];
                     median += period[d / 2 + 1];
-                }
-                else  median = period[d / 2 + 1] * 2; // Медиана у массива с нечетным колличеством элементов
-                if (expenditure[d+i] >= median){
+                } else median = period[d / 2 + 1] * 2; // Медиана у массива с нечетным колличеством элементов
+                System.out.println(Arrays.toString(period) + " | " + expenditure[d + 1] + " больше (или равен)" + median + " ?");
+                if (expenditure[d + i] >= median) {
+                    System.out.println("Да");
                     count++;
-                }
+                } else System.out.println("Нет");
             }
-            System.out.println(count);
+            System.out.println("Ответ : " + count);
             return count;
-        }
-        else return 0;
+        } else return 0;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -80,27 +80,38 @@ class Solution {
 
         String[] nd = scanner.nextLine().split(" ");
 
-        int n = Integer.parseInt(nd[0]);
-
-        int d = Integer.parseInt(nd[1]); // Длинна проверочного периода
-
-        int[] expenditure = new int[n];
-
         String[] expenditureItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < n; i++) {
-            int expenditureItem = Integer.parseInt(expenditureItems[i]);
-            expenditure[i] = expenditureItem;
-        }
-
-        Bank bank;
-        bank = (x, y) -> activityNotifications(expenditure, d);
-
-        int r = bank.method(d, expenditure);
+        AtomicInteger r = new AtomicInteger(); // Добавил внешнюю перемененную, чтоб вытягивать значение из потока
 
 
-        bufferedWriter.write(String.valueOf(r));
+        Thread tr = new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + "  - поток запущен");
+
+                int n = Integer.parseInt(nd[0]);
+
+                int d = Integer.parseInt(nd[1]); // Длинна проверочного периода
+
+                int[] expenditure = new int[n];
+
+                for (int i = 0; i < n; i++) {
+                    int expenditureItem = Integer.parseInt(expenditureItems[i]);
+                    expenditure[i] = expenditureItem;
+                }
+
+                r.set(activityNotifications(expenditure, d));
+                System.out.println(Thread.currentThread().getName() + "  - поток остановлен");
+            }
+            catch (Exception exception){
+                System.out.println("Error!");
+            }
+        });
+        tr.start();
+
+
+        bufferedWriter.write(String.valueOf(r.get()));
         bufferedWriter.newLine();
 
         bufferedWriter.close();
@@ -109,6 +120,25 @@ class Solution {
     }
 
 }
-interface Bank{
-    int method(int d, int[] expenditure);
-}
+//@FunctionalInterface
+//interface Bank{
+//    int method(int d, int[] expenditure);
+//}
+//
+//class MyThread extends Thread{
+//
+//    public int[] expenditure;
+//    public int d;
+//
+//    public void setD(int d){ this.d = d;} // Прочитать переменную d
+//    public void setExpenditure(int[] expenditure){ this.expenditure = expenditure;} // Прочитать массив
+//
+//    @Override
+//    public void run() {
+//        super.run();
+//        Bank bank;
+//    }
+//
+//
+//
+//}
